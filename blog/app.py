18015468de79e22ea89c6800c2b7190d306b2094
tigerhu <huxy99@yahoo.com>
2020-10-26 08:48:57 -0500

    Add Templates & Static

--- blog/app.py
+++ blog/app.py
@@ -1,17 +1,32 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
+
+posts = [
+    {
+        'author': 'Tiger Hu',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'October 26, 2020'
+    },
+    {
+        'author': 'Sam Chen',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'October 26, 2020'
+    }
+]
 
 
 @app.route("/")
 @app.route("/home")
 def home():
-    return "<h1>Home Page</h1>"
+    return render_template('home.html', posts=posts)
 
 
 @app.route("/about")
 def about():
-    return "<h1>About Page</h1>"
+    return render_template('about.html', title='About')
 
 
 if __name__ == '__main__':
